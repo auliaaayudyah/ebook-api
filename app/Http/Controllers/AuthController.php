f729e4a47363997106369a31a63d4155c1009008
auliaaayudyah <auliaaayudyah@gmail.com>
2021-10-13 08:16:31 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
   public function me() {
-    return ['NIS' => 3103119034,
+    return ['NIS' => 3103119092,
         'name' => 'Aulia Ayu Dyah Lestari',
         'gender' => 'Female',
         'phone' => '081231888226',
-        'absen' => '06',
-        'class' => 'XII RPL 1'];
+        'class' => 'XII RPL 1'
+    ];
+  }
+  public function register(Request $request) {
+    $fields = $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|string|unique:users,email',
+        'password' => 'required|string|confirmed'
+    ]);
+
+    $user = User::create([
+        'name' => $fields['name'],
+        'email' => $fields['email'],
+        'password' => bcrypt($fields['password'])
+    ]);
+
+    $token = $user->createToken('myapptoken')->plainTextToken;
+
+    $response = [
+        'user' => $user,
+        'token' => $token
+    ];
+
+    return response($response, 201);
+}
+
+  public function login(Request $request) {
+      $fields = $request->validate([
+          'email' => 'required|string',
+          'password' => 'required|string'
+      ]);
+
+      // Check email
+      $user = User::where('email', $fields['email'])->first();
+
+      // Check password
+      if(!$user || !Hash::check($fields['password'], $user->password)) {
+          return response([
+              'message' => 'Gagal Login'
+          ], 401);
+      }
+
+      $token = $user->createToken('myapptoken')->plainTextToken;
+
+      $response = [
+          'user' => $user,
+          'token' => $token
+      ];
+
+      return response($response, 201);
+  }
+
+  public function logout(Request $request) {
+      auth()->user()->tokens()->delete();
+
+      return [
+          'message' => 'Logged out'
+      ];
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,15 +15,28 @@ use App\Http\Controllers\AuthorController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+*/     
+//Route::get('books', [BookController::class, 'index']);
+//Route::post('books', [BookController::class, 'store']);
+//Route::get('books/{id}', [BookController::class, 'show']);
+//Route::put('books/{id}', [BookController::class, 'update']);
+//Route::delete('books/{id}', [BookController::class, 'destroy']);
+//Route::resource('authors', AuthorController::class);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+   
+    Route::post('/books', [BookController::class, 'store']);
+    Route::get('/books/{id}', [BookController::class, 'show']);
+    Route::put('/books/{id}', [BookController::class, 'update']);
+    Route::delete('/books/{id}', [BookController::class, 'destroy']);
+    Route::resource('/authors', AuthorController::class)->except('edit','create');
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::get('books', [BookController::class, 'index']);
-Route::post('books', [BookController::class, 'store']);
-Route::get('books/{id}', [BookController::class, 'show']);
-Route::put('books/{id}', [BookController::class, 'update']);
-Route::delete('books/{id}', [BookController::class, 'destroy']);
-Route::resource('authors', AuthorController::class);
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/books/search/{title}', [BookController::class, 'search']);
+Route::get('/authors/search/{name}', [AuthorController::class, 'search']);
+Route::get('/books', [BookController::class, 'index']);
